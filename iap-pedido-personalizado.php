2bda079346e4ac6cace2583b306a6349fe8669c1
willianVx <willianvoliveiravx@gmail.com>
2018-05-09 09:36:34 -0300

    Porta retrato - mobile 3

--- iap-pedido-personalizado.php
+++ iap-pedido-personalizado.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://instaarts.com.br
  * Description: Esse plugin implementa a ferramenta da Adobe (Creative SDK) para edição de imagens, mais menu onde o usuário pode fazer seu pedido personalizado escolhendo tamanho, acabamento e moldura. 
  * Author URI: Willian De Oliveira 
- * Version: 1.3.6 BETA
+ * Version: 1.5 BETA
  * Licence: GPL2
  */
 if ( ! defined( 'ABSPATH' ) ) {
