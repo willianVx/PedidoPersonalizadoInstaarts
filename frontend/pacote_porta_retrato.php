4dd88bb32cf35c8d374db2e34d684970227da109
willianVx <willianvoliveiravx@gmail.com>
2018-09-27 10:05:17 -0300

    Problemas com o PHP

--- frontend/pacote_porta_retrato.php
+++ frontend/pacote_porta_retrato.php
@@ -1,47 +1,20 @@
 <?php
 //inclui sidebar para o pacote de porta retratos
     function sidebar_porta_retrato(){
-        ?>
-            
+ ?>
             <div class="sidebar_container">
-           
             <div id="sidebar_porta_retrato">
-           
             <div id="porta_retrato_module">
                 <ul id="porta_retrato">
                 </ul>
-                <!-- 
-                <ul id="porta_retrato">
-                    <script id="porta_retrato_template" type="text/template">
-                        {{#portaRetrato}}
-                            
-                            <li>    
-                                    <canvas id={{.}} width="69" height="53"></canvas>
-
-                                    <div id="info_painel_texto">
-                                        {{texto1}}<br>
-                                        {{texto2}}
-                                        <span>{{tamanho}}</span>
-                                    </div>
-                                
-                                <! <span>{{.}}</span> --> <!--
-                                 <i class="del glyphicon glyphicon-remove"></i> 
-
-                            </li>
-                        {{/portaRetrato}}
-                    </script>
-                </ul>
-                -->
             </div>
                         <div id="add_novo_quadro">
                             <img class="img_add_novo_quadro" src="<?php echo plugins_url( 'img/add_novo_quadro.png', __FILE__ ); ?>" border="0" />
                             <div id="quadros_restantes">Adicionar imagem <span id="quadros_restantes_numero">0 </span>de 5</div>
                         </div>
-                        
             </div>
                 <button class="btn btn-default b_comprar_porta_retrato">Comprar<span></span></button>
             </div>
-
             <!-- Modal -->
             <div class="modal fade" id="porta_retrato_loading" tabindex="-1" role="dialog" aria-hidden="true">
                 <div class="modal-dialog" role="document">
@@ -57,13 +30,12 @@
                     </div>
                 </div>
             </div>
-
-        <?php
+<?php
     }
 //versao sidebar porta retrato mobile
 
     function sidebar_porta_retrato_mobile(){
-        ?>  
+?>  
             <div class="m_botton_menu">
                 <div id="m_iap_crop_porta_retrato" class="m_cortar_adicionar"><span class="glyphicon glyphicon-scissors"></span><span class="crop_texto"> Cortar e adicionar</span></div>
 
@@ -89,8 +61,5 @@
                 <div class="m_reiniciar_porta_retrato glyphicon glyphicon-trash"></div>
 
             </div>
-           
-        <?php
-    }
-
-?> 
+<?php
+        }
